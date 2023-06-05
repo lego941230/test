0121f03d256f20bbca9fc87727448fb0f793f53b
lego941230 <132253567+lego941230@users.noreply.github.com>
2023-06-05 14:38:44 +0800

    Update script.js

--- 111/dist/script.js
+++ 111/dist/script.js
@@ -1,1 +1,8 @@
 document.write("HELLO我是陳垣凱 ")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
